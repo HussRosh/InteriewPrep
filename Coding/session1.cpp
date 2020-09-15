4df20048e7bcadc898d5cb5be41840a23dbebfd1
HussRosh <70639375+HussRosh@users.noreply.github.com>
2020-09-15 15:03:45 +0530

    Add files via upload

---
+++ Coding/session1.cpp
@@ -0,0 +1,249 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// (return type) (function name) (arguments)
+void Roshni(){
+	cout<<"I want to eat you . "<<endl;
+}
+
+int Hussain (int &a1 ,  int b1 , int c1) {
+	a1=0;
+	return a1+b1+c1;  
+}
+
+
+
+
+
+
+int main()
+{
+
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt","r",stdin);
+	// freopen("ouput.txt","w",stdout);
+	// #endif
+
+
+
+
+
+	//data types 
+
+	//numeric data types 
+	int rosh_i   = 1 ; //whole number values 10^9
+	long long rosh_l = 10000000; //veryyyyy big whole numbers
+	
+	//decimal point values
+	float rosh_fl = 1.2 ;
+	double rosh_d = 8.8888888;
+
+
+	//Character data type
+	char a = 'a';
+	char rosh_c = 'Z';
+
+	//Boolean 
+	bool rosh_t = true; // 1 == true
+	rosh_t = 1 ;
+
+	bool rosh_f = false; // 0 == false
+	rosh_f = 0 ; 
+	
+
+
+	// cin >> rosh_i >> rosh_d >> rosh_c ;
+
+	// cout << rosh_i <<" "<< rosh_d <<" "<<rosh_c << endl;
+	// cout <<"Rosh is smart \n " ;
+	// cout <<"Rosh is beautiful  ."<<rosh_fl << "\n";
+
+
+
+//CONDITION 
+
+// what does mod do  ?
+// 5%2 -> 1 -> odd
+// 6%2 -> 0 -> even 
+
+// int x ;
+// cin>>x; 
+
+// if(x%2==0){
+// 	cout<<"EVEN"<<endl;
+// }
+// else{
+// 	cout<<"ODD"<<endl;
+// }
+
+
+
+//LOOPS 
+
+int t = 10 ;
+
+// while(t > 0){
+	
+// 	//body
+// 	cout<< "Roshni Sah "<<endl ;
+
+// 	//condition will be re-evaluated
+
+	// t = t - 1 ;
+	// t -= 1 ;
+	// t-- ;
+// }
+
+
+// for(initialisation  ; condition  ; re-eval-part ){
+// 	//body / logical 
+// }
+
+
+// for (int i = 0; i < t ; ++i)
+// {
+
+// 	cout << " I love Wai Wai " <<endl ;
+
+
+// }
+
+
+// pre- and post - 
+
+// ++i => pre- increment (i=i+1)
+// i++ => post-increment (i=i+1)
+
+
+// int i  = 10  ;
+
+//  int x = ++i  + 10 ;
+
+// cout<<"x = "<<x<<" i="<<i<<endl ; 
+
+
+// int  y  = i++  + 10 ;
+
+// cout<< "y = "<< y << " i=" << i <<endl;
+
+
+// Roshni() ; 
+//always write function above ya upar whereever it is called
+
+
+// int a1 = 1 , b1 = 2 , c1 = 3 ; 
+
+// cout<< Hussain(a1,b1,c1) <<endl;
+
+// cout<< a1 <<endl ; 
+
+
+
+
+
+
+//Arrays 
+
+//collection of SAME type
+
+// int arr[] = {1,2,3,4,5,6} ;  
+
+// int n ;
+// cin>>n;
+
+// int arr1[n];
+
+// for(int i=0;i<n;i++){
+// 	arr1[i] = i ; 
+// }
+
+// for(int i=0;i<n;i++){
+// 	cout<<arr1[i]<<endl; 
+// }
+
+// int rows , cols ;
+// cin>>rows >> cols ;
+
+// int arr2[rows][cols];
+
+// for(int row = 0 ; row < rows ;row++){
+// 	for(int col = 0 ; col < cols ; col++ ){
+// 		cin >> arr2[row][col] ; 
+// 	}
+// }
+
+// for(int row = 0 ; row < rows ;row++){
+
+// 	for(int col = 0 ; col < cols ; col++ ){
+// 		cout << arr2[row][col] <<" " ; 
+// 	}
+
+// 	cout<<endl;
+// }
+
+
+
+//String
+
+//write a program to count the number of vowels
+// string tmp = "Roshni Sah"  ;
+
+// int vowels = 0 ; 
+
+// for(int i = 0 ; i < tmp.size() ; i++ ){
+	
+// 	char current_char = tmp[i] ;
+
+// 	if(current_char == 'a' || current_char =='e' ||  current_char =='i' ||  current_char =='o' ||  current_char =='u'){
+// 		vowels++;
+// 	}
+
+// }		
+
+// cout<<vowels<<endl;
+
+
+// vector<int> vec ;
+
+
+// vec.push_back(1);
+// vec.push_back(2);
+
+// vec.push_back(3);
+// vec.push_back(4);
+
+// // vec : 
+// // vec : 1
+// // vec : 1 2
+// // vec : 1 2 3
+// // vec : 1 2 3 4
+
+
+// for(int i=0;i<vec.size();i++){
+// 	cout<<vec[i]<<endl;
+// }
+
+
+// string s ;
+
+// // cin >> s ;
+
+// getline(cin,s);
+
+// cout<<s<<endl;
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
+
+
+
